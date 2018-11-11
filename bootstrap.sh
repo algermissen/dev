392197d55a284e4e96f3c91564e542d571de9490
Jan Algermissen <algermissen@acm.org>
2018-11-11 21:00:47 +0000

    Backup

--- bootstrap.sh
+++ bootstrap.sh
@@ -77,3 +77,5 @@ echo 'export GOPATH=/home/dev/go' >> ~/.profile
 echo 'export PATH=$PATH:$GOPATH/bin:$GOROOT/bin' >> ~/.profile
     
 
+https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-16-04
+
